9a0042ab921e731323c63b5c05f7fe7a00e86ba3
Ashwani Rajput <ashwanir461@gmail.com>
2022-08-25 09:26:37 +0530

    complete code

---
+++ src/derived/AutomaticCVT.java
@@ -0,0 +1,22 @@
+package derived;
+
+public class AutomaticCVT extends AMT {
+    public AutomaticCVT(String transmissionType, String modelNo, int noOfGears) {
+        super(transmissionType, modelNo, noOfGears);
+    }
+
+    @Override
+    public double[] getGearRatio() {
+        return super.getGearRatio();
+    }
+
+    @Override
+    public void setGearRatio(double[] gearRatio) {
+        super.setGearRatio(gearRatio);
+    }
+
+    @Override
+    public void showSpecs() {
+        super.showSpecs();
+    }
+}
---
+++ src/derived/Manual.java
@@ -0,0 +1,24 @@
+package derived;
+
+import base.Transmission;
+
+public class Manual extends Transmission {
+    public Manual(String transmissionType, String modelNo, int noOfGears) {
+        super(transmissionType, modelNo, noOfGears);
+    }
+
+    @Override
+    public double[] getGearRatio() {
+        return super.getGearRatio();
+    }
+
+    @Override
+    public void setGearRatio(double[] gearRatio) {
+        super.setGearRatio(gearRatio);
+    }
+
+    @Override
+    public void showSpecs() {
+        super.showSpecs();
+    }
+}
